348b74239a3020b31b2f3bc2af4137a5c36b7187
root <esouidi@enssat.fr>
2023-02-09 14:49:40 +0100

    modifié :         mail_processing.py

--- mail_processing.py
+++ mail_processing.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Script python pour gérer et traiter les mails entrant (récupérés par le démon)
+# Script python pour gerer et traiter les mails entrant (recuperes par le demon)
 
 import smtplib 
 import sys
@@ -24,16 +24,16 @@ mail body
 
 content=[]
 for line in sys.stdin:
-    content.append(line.rstrip())       # Récupération du mail par lignes
+    content.append(line.rstrip())       # Recuperation du mail par lignes
 
-sujet = content[2][9:]                                          # Récupération du sujet du mail
-destinataire = re.findall(r"<(.*?)>", content[3])[0]            # Récupération du destinataire (To: <meldiffere+toto*enssat.fr-2023/02/09T14/20/00@enssat.fr>) 3ème ligne
-expediteur = re.findall(r"<(.*?)>", content[7])[0]              # Récupération de l'expéditeur (From: Souidi)
-message = content[-1]                                           # Récupération du body du mail
+sujet = content[2][9:]                                          # Recuperation du sujet du mail
+destinataire = re.findall(r"<(.*?)>", content[3])[0]            # Recuperation du destinataire (To: <meldiffere+toto*enssat.fr-2023/02/09T14/20/00@enssat.fr>) 3eme ligne
+expediteur = re.findall(r"<(.*?)>", content[7])[0]              # Recuperation de l'expediteur (From: Souidi)
+message = content[-1]                                           # Recuperation du body du mail
 
-try:                                                                           # Le mail est à differer
+try:                                                                           # Le mail est a differer
     dest_original_asterisk = re.findall(r"\+(.*?)-", destinataire)[0]          # Extraction de l'adresse de destination sans "To: " avec *
-    date_cible_str = re.findall(r"-(.*?)@", destinataire)[0]                   # Exctraction de l'heure d'arrivée voulue du mail
+    date_cible_str = re.findall(r"-(.*?)@", destinataire)[0]                   # Exctraction de l'heure d'arrivee voulue du mail
     print(date_cible_str)
     print(dest_original_asterisk)
 
@@ -41,13 +41,13 @@ except:
     print("erreur dans l'adresse mail")
 
 else:
-    print("mail à différer")
+    print("mail a differer")
 
     dest_original = dest_original_asterisk.replace('*','@')                         # Remplacement de l'* par @ dans l'adresse de destination
-    date_cible = datetime.datetime.strptime(date_cible_str, "%Y/%m/%dT%H/%M/%S")    # Récupération de la date cible en format ISO
-    time_cible = int(time.mktime(date_cible.timetuple()))                           # Conversion de la date cible en timestamp UNIX pour pouvoir faire un sleep derrière
+    date_cible = datetime.datetime.strptime(date_cible_str, "%Y/%m/%dT%H/%M/%S")    # Recuperation de la date cible en format ISO
+    time_cible = int(time.mktime(date_cible.timetuple()))                           # Conversion de la date cible en timestamp UNIX pour pouvoir faire un sleep derriere
 
-    time_to_sleep = time_cible - int(time.time())                                   # Temps de sommeil :  différence entre le temps actuel et le temps cible
+    time_to_sleep = time_cible - int(time.time())                                   # Temps de sommeil :  difference entre le temps actuel et le temps cible
 
     time.sleep(time_to_sleep)
 
